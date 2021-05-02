556dec49cae089d9c5b4b17d333284e33bfcf7a2
madeinchema <chemavg1@gmail.com>
2021-05-02 22:42:38 +0200

    add php script to fetch data bypassing CORS

---
+++ php/index.php
@@ -0,0 +1,9 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+echo file_get_contents('https://uptime-tracker.skywire.skycoin.com/uptimes');
+
+?>
