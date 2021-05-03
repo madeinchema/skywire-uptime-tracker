fd6a39cfd40bc867ce0f9d759921d365524b6163
madeinchema <chemavg1@gmail.com>
2021-05-03 11:58:08 +0200

    add VisorSearch typed comp

---
+++ components/VisorSearch.tsx
@@ -0,0 +1,19 @@
+import { Input } from '@chakra-ui/input';
+import { Dispatch, SetStateAction } from 'react';
+
+type Props = {
+  value: string;
+  setValue: Dispatch<SetStateAction<string | undefined>>;
+};
+
+const VisorSearch = (props: Props) => {
+  const { value, setValue } = props;
+
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return <Input value={value} onChange={handleInputValue} />;
+};
+
+export default VisorSearch;
