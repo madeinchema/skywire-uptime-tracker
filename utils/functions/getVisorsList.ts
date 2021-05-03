3cc9232f1acefc1983612d9706732cdb20654fa9
madeinchema <chemavg1@gmail.com>
2021-05-03 11:47:17 +0200

    handle use of fake data in getVisorsList func

--- utils/functions/getVisorsList.ts
+++ utils/functions/getVisorsList.ts
@@ -1,4 +1,11 @@
-const getVisorsList = async () => {
+import sampleVisorsUptimeData from '../sample-visors-uptime-data';
+
+const getVisorsList = async (useFakeData: boolean = false) => {
+  if (useFakeData) {
+    return new Promise((res, rej) => {
+      res(sampleVisorsUptimeData);
+    });
+  }
   const visorsList = await fetch('http://localhost:8080/visors', {
     headers: { 'Content-Type': 'application/json' },
   }).then((res) => res.json());
