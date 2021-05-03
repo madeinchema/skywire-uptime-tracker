49eb8380624c789fe74e53739630ec41f4cb954f
madeinchema <chemavg1@gmail.com>
2021-05-03 13:10:50 +0200

    add _document, pass ColorModeScript

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import { ColorModeScript } from '@chakra-ui/color-mode';
+import theme from '@chakra-ui/theme';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
