f510c959d93caabb13b9d335fba88b0a9f38d6a5
madeinchema <chemavg1@gmail.com>
2021-05-03 16:34:46 +0200

    manage horizontal overflow

--- components/VisorsUptimeTable.tsx
+++ components/VisorsUptimeTable.tsx
@@ -7,34 +7,36 @@ type Props = {
 
 const VisorsUptimeTable = ({ dataSource }: Props) => {
   return (
-    <Table size="sm" variant="striped">
-      <Thead>
-        <Th width="80px">Online</Th>
-        <Th>Key</Th>
-        <Th isNumeric>Percentage</Th>
-        <Th isNumeric>Uptime</Th>
-        <Th isNumeric>Downtime</Th>
-      </Thead>
-      <Tbody>
-        {dataSource &&
-          dataSource.map((visor) => (
-            <Tr key={visor.key}>
-              <Td>
-                <Box
-                  width={3}
-                  height={3}
-                  borderRadius="100%"
-                  bgColor={visor.online ? 'green' : 'red'}
-                />
-              </Td>
-              <Td>{visor.key}</Td>
-              <Td isNumeric>{visor.percentage}</Td>
-              <Td isNumeric>{visor.uptime}</Td>
-              <Td isNumeric>{visor.downtime}</Td>
-            </Tr>
-          ))}
-      </Tbody>
-    </Table>
+    <Box overflowX="auto" maxWidth="100%">
+      <Table size="sm" variant="striped">
+        <Thead>
+          <Th width="80px">Online</Th>
+          <Th>Key</Th>
+          <Th isNumeric>Percentage</Th>
+          <Th isNumeric>Uptime</Th>
+          <Th isNumeric>Downtime</Th>
+        </Thead>
+        <Tbody>
+          {dataSource &&
+            dataSource.map((visor) => (
+              <Tr key={visor.key}>
+                <Td>
+                  <Box
+                    width={3}
+                    height={3}
+                    borderRadius="100%"
+                    bgColor={visor.online ? 'green' : 'red'}
+                  />
+                </Td>
+                <Td>{visor.key}</Td>
+                <Td isNumeric>{visor.percentage}</Td>
+                <Td isNumeric>{visor.uptime}</Td>
+                <Td isNumeric>{visor.downtime}</Td>
+              </Tr>
+            ))}
+        </Tbody>
+      </Table>
+    </Box>
   );
 };
 
