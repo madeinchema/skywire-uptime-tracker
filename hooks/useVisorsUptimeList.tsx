08806229c37fb42335caa17b09f2a2f406f47fcb
madeinchema <chemavg1@gmail.com>
2021-05-03 11:48:06 +0200

    add hook, handle data fetching

---
+++ hooks/useVisorsUptimeList.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { VisorUptime } from '../interfaces';
+import { saveVisorsUptimeData } from '../state/slices/visorsUptimeSlice';
+import { getVisorsList } from '../utils/functions/getVisorsList';
+
+const useVisorsUptimeList = () => {
+  const [visorsUptimeList, setVisorsUptimeList] = useState<
+    VisorUptime[] | undefined
+  >(undefined);
+  const visorsUptimeListFromStore = useSelector(
+    (state) => state.visorsUptime.visors
+  );
+  const dispatch = useDispatch();
+  const USE_FAKE_DATA = true;
+
+  useEffect(() => {
+    if (visorsUptimeListFromStore.length < 1) {
+      getVisorsList(USE_FAKE_DATA).then((data) =>
+        dispatch(saveVisorsUptimeData(data))
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    setVisorsUptimeList(visorsUptimeListFromStore);
+  }, [visorsUptimeListFromStore]);
+
+  const handlers = React.useMemo(
+    () => ({
+      handleVisorsUptimeList: (): void => console.log('handleVisorsUptimeList'),
+    }),
+    []
+  );
+
+  return { visorsUptimeList, handlers };
+};
+
+export default useVisorsUptimeList;
