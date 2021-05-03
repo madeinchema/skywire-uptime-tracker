4a0218849a1c0a490caed8966e764ce53abeb48a
madeinchema <chemavg1@gmail.com>
2021-05-03 17:50:26 +0200

    add custom theme for the Table component

--- theme/theme.js
+++ theme/theme.js
@@ -1,7 +1,29 @@
 import { extendTheme } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools';
 
+/**
+ * Table component
+ */
+const Table = {
+  variants: {
+    custom: (props) => ({
+      tr: {
+        _odd: {
+          background: mode('gray.100', 'gray.800')(props),
+        },
+        _even: { background: mode('gray.200', 'gray.900')(props) },
+      },
+    }),
+  },
+};
+
+/**
+ * Custom theme
+ */
 const theme = extendTheme({
+  components: {
+    Table,
+  },
   styles: {
     global: (props) => ({
       body: {
