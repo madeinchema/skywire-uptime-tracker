c14248d6808ded4daea1a74890d4e1a861664015
madeinchema <chemavg1@gmail.com>
2021-05-04 23:16:55 +0200

    add getMyVisorsList func, return sample data for now

--- utils/functions/getVisorsList.ts
+++ utils/functions/getVisorsList.ts
@@ -1,6 +1,7 @@
+import sampleMyVisorsData from '../sample-my-visors-data';
 import sampleVisorsUptimeData from '../sample-visors-uptime-data';
 
-const getVisorsList = async (useFakeData: boolean = false) => {
+const getVisorsList = async (useFakeData = false): Promise<any> => {
   if (useFakeData) {
     return new Promise((res) => {
       res(sampleVisorsUptimeData);
@@ -12,4 +13,19 @@ const getVisorsList = async (useFakeData: boolean = false) => {
   return visorsList;
 };
 
-export { getVisorsList };
+const getMyVisorsList = async (useFakeData = false): Promise<any> => {
+  if (useFakeData) {
+    return new Promise((res) => {
+      res(sampleMyVisorsData);
+    });
+  }
+  return new Promise((res) => {
+    res(sampleMyVisorsData);
+  });
+  // const visorsList = await fetch('http://localhost:8080/visors', {
+  //   headers: { 'Content-Type': 'application/json' },
+  // }).then((res) => res.json());
+  // return visorsList;
+};
+
+export { getVisorsList, getMyVisorsList };
