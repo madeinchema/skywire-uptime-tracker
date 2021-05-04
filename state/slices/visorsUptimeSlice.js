c4b5a26014bfbd5cad5bddf41fcaca86254df97c
madeinchema <chemavg1@gmail.com>
2021-05-04 23:26:31 +0200

    add "status" to the visorsUptime state

--- state/slices/visorsUptimeSlice.js
+++ state/slices/visorsUptimeSlice.js
@@ -3,11 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 export const visorsUptimeSlice = createSlice({
   name: 'visorsUptime',
   initialState: {
+    status: 'loading',
     visors: [],
   },
   reducers: {
     saveVisorsUptimeData: (state, action) => {
       state.visors = action.payload;
+      state.status = 'success';
     },
   },
 });
