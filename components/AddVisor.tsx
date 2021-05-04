d5cbfc3c575fba672dc46c21ce3970fbae6f68c1
madeinchema <chemavg1@gmail.com>
2021-05-04 21:23:15 +0200

    add & handle inputs, clean component

--- components/AddVisor.tsx
+++ components/AddVisor.tsx
@@ -1,57 +1,83 @@
 import { Button } from '@chakra-ui/button';
 import { Input } from '@chakra-ui/input';
-import { Container, Flex, HStack } from '@chakra-ui/layout';
+import { Container, Flex, HStack, Text, VStack } from '@chakra-ui/layout';
 import { useState } from 'react';
 import useVisorsUptimeList from '../hooks/useVisorsUptimeList';
+import { VisorUptime } from '../interfaces';
+import VisorCard from './VisorCard';
+
+interface AddVisorInput {
+  label: string;
+  key: string;
+}
 
 const AddVisor = (): JSX.Element => {
   const { visorsUptimeList } = useVisorsUptimeList();
-  const [inputValue, setInputValue] = useState<string | undefined>(undefined);
+  const [inputValues, setInputValues] = useState<AddVisorInput | undefined>(
+    undefined
+  );
+  const [visorStatusChecked, setVisorStatusChecked] = useState<
+    VisorUptime | undefined
+  >(undefined);
 
-  const checkIsVisorListed = (key: string): void => {
-    const isVisorListed =
-      visorsUptimeList?.findIndex((visor) => visor.key === key) !== -1;
-    console.log({ isVisorListed });
-  };
-  const myVisor =
-    '020011587bf42a45b15f40d6783f5e5320a69a97a7298382103b754f2e3b6b63e9';
-
-  const isVisorOnline = (key: string): void => {
-    const checkIsVisorOnline =
-      visorsUptimeList?.findIndex(
-        (visor) => visor.key === key && visor.online === true
-      ) !== -1;
-    console.log({ checkIsVisorOnline });
+  const checkVisorStatus = (): void => {
+    const visorStatus =
+      inputValues?.key &&
+      visorsUptimeList?.find((visor) => visor.key === inputValues.key);
+    if (visorStatus) setVisorStatusChecked(visorStatus);
   };
 
-  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setInputValue(e.target.value);
-  };
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    setInputValues((prevState): any => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
 
   return (
-    <Container px={2} maxW="container.lg">
+    <Container px={2} maxW="container.md">
       <Flex direction="column">
-        Visor: {myVisor}
-        <Button onClick={() => checkIsVisorListed(myVisor)}>
-          Is visor listed?
-        </Button>
-        <Button onClick={() => checkIsVisorListed(myVisor)}>
-          Is visor Online?
-        </Button>
-        <p>{inputValue}</p>
-        <HStack>
-          <Flex direction="column">
-            Label
-            <Input value={inputValue} onChange={handleInputValue} />
+        <VStack>
+          <Flex direction="column" w="100%">
+            <Text>Label</Text>
+            <Input
+              name="label"
+              value={inputValues?.label}
+              onChange={handleInputValue}
+            />
           </Flex>
-          <Flex direction="column" width="100%">
-            Public key
-            <Input value={inputValue} onChange={handleInputValue} />
+          <Flex direction="column" w="100%">
+            <Text>Public key</Text>
+            <Input
+              name="key"
+              value={inputValues?.key}
+              onChange={handleInputValue}
+            />
           </Flex>
-          <Flex direction="column" align="flex-end" width="100%">
-            <Button>Add visor</Button>
-          </Flex>
-        </HStack>
+          <HStack direction="column" w="100%">
+            <Button
+              w="100%"
+              variant="outline"
+              colorScheme="blue"
+              onClick={checkVisorStatus}
+            >
+              Check visor status
+            </Button>
+            <Button w="100%" colorScheme="blue">
+              Add visor
+            </Button>
+          </HStack>
+          {visorStatusChecked && (
+            <VisorCard
+              visor={{
+                key: visorStatusChecked.key,
+                uptime: visorStatusChecked.uptime,
+                downtime: visorStatusChecked.downtime,
+                percentage: visorStatusChecked.percentage,
+                online: visorStatusChecked.online,
+              }}
+            />
+          )}
+        </VStack>
       </Flex>
     </Container>
   );
