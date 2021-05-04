f183ccabbc6672941ed5d95f2161ae8bc0a4c45c
madeinchema <chemavg1@gmail.com>
2021-05-04 23:25:21 +0200

    add labels to myVisors sample data

--- utils/sample-my-visors-data.ts
+++ utils/sample-my-visors-data.ts
@@ -1,26 +1,30 @@
 export default [
   {
+    label: 'Skyminer-1-1',
     key: '020011587bf42a45b15f40d6783f5e5320a69a97a7298382103b754f2e3b6b63e9',
   },
   {
+    label: 'Skyminer-1-2',
     key: '02001728a88c27b6fa73ebc969dccdbcbd1d4393f267ea10fff2ed8d5eccaca0a4',
   },
   {
+    label: 'Skyminer-1-3',
     key: '02004a94f317f3a7f857b4531906e72a0691bf1853e07d17e6632e40240bb11ee1',
   },
   {
+    label: 'Skyminer-1-4',
     key: '02004aa9e2caea09fa20d9fb701737627e8df14a0c3ed082416f23857465982757',
   },
   {
+    label: 'Skyminer-1-5',
     key: '02006c60e7ed61006c7e19aab75362f3b36ddfc0c5e6c845533470de1445023bae',
   },
   {
+    label: 'Skyminer-1-6',
     key: '020088ac4b5a8ffc8409e64e859e1966b21ca02b31605cfaf0b8dbb6242051b642',
   },
   {
+    label: 'Skyminer-1-7',
     key: '0200adbb247a99bc321498ef31f654a126ca395a04a9bded5954a1236ed6532b14',
-  },
-  {
-    key: '0200ae2e5e6b16ce6c47c819dd9a9f8305057271146331b7844dda0fee88879087',
   },
 ];
