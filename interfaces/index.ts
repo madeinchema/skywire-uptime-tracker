0793a62e9695cc628ee53b1467fdb91c41d8d595
madeinchema <chemavg1@gmail.com>
2021-05-04 21:57:01 +0200

    add VisorKey, VisorLabel & MyVisorUptime types, update them

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,11 +1,17 @@
-export type Visor = {
-  pk: string;
+export type VisorKey = string;
+export type VisorLabel = string;
+
+export type MyVisor = {
+  key: VisorKey;
+  label: VisorLabel;
 };
 
 export type VisorUptime = {
-  key: string;
+  key: VisorKey;
   uptime: number;
   downtime: number;
   percentage: number;
   online: boolean;
 };
+
+export type MyVisorUptime = MyVisor & VisorUptime;
