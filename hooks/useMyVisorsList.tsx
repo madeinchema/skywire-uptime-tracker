e9e3ab724e0bf06e075dcb4741cc33382ce5fbd7
madeinchema <chemavg1@gmail.com>
2021-05-05 13:02:58 +0200

    add "addNewVisor" handler func & improve code

--- hooks/useMyVisorsList.tsx
+++ hooks/useMyVisorsList.tsx
@@ -1,57 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
-import { MyVisorUptime, VisorUptime } from '../interfaces';
-import { saveMyVisorsData } from '../state/slices/myVisorsSlice';
-import { getMyVisorsList } from '../utils/functions/getVisorsList';
+import React, { useEffect, useState } from 'react'
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux'
+import { MyVisor, MyVisorUptime, VisorUptime } from '../interfaces'
+import { addNewVisor, saveMyVisorsData } from '../state/slices/myVisorsSlice'
+import { getMyVisorsList } from '../utils/functions/getVisorsList'
 
 interface UseMyVisorsList {
-  myVisorsList: VisorUptime[] | undefined;
+  myVisorsList: VisorUptime[] | undefined
   handlers: {
-    handleMyVisorsList: () => void;
-  };
+    addNewVisor: (visor: MyVisor) => void
+  }
 }
 
 const useMyVisorsList = (): UseMyVisorsList => {
   const [myVisorsList, setMyVisorsList] = useState<MyVisorUptime[] | undefined>(
     undefined
-  );
+  )
   const myVisorsSelector = useSelector(
     (state: RootStateOrAny) => state.myVisors.visors
-  );
+  )
   const visorsUptimeListSelector = useSelector(
     (state: RootStateOrAny) => state.visorsUptime.visors
-  );
-  const dispatch = useDispatch();
-  const USE_FAKE_DATA = true;
+  )
+  const dispatch = useDispatch()
 
-  console.log({ myVisorsSelector });
-  console.log({ myVisorsList });
-
-  // TODO: Make all this readable
+  /**
+   * Get and save data to store when its empty
+   */
   useEffect(() => {
-    const shouldGetData = myVisorsSelector.length < 1;
+    const shouldGetData = myVisorsSelector.length < 1
     if (shouldGetData) {
-      getMyVisorsList(USE_FAKE_DATA).then((data) =>
+      getMyVisorsList('USE_FAKE_DATA').then((data) =>
         dispatch(saveMyVisorsData(data))
-      );
+      )
     }
-  }, [USE_FAKE_DATA, dispatch, myVisorsSelector.length]);
+  }, [dispatch, myVisorsSelector.length])
 
+  /**
+   * Update myVisorsList on store changes
+   */
   useEffect(() => {
     const myVisorsDataList = visorsUptimeListSelector.filter((visor) => {
-      return myVisorsSelector.some((myVisor) => visor.key === myVisor.key);
-    });
-    setMyVisorsList(myVisorsDataList);
-  }, [myVisorsSelector, visorsUptimeListSelector]);
+      return myVisorsSelector.some((myVisor) => visor.key === myVisor.key)
+    })
+    setMyVisorsList(myVisorsDataList)
+  }, [myVisorsSelector, visorsUptimeListSelector])
+
+  const checkVisorStatus = (): void => {
+    const visorStatus = visorsUptimeListSelector?.find(
+      (visor) => visor.key === inputValues.key
+    )
+    if (visorStatus) setVisorStatusSelected(visorStatus)
+  }
+
+  const handleLabelSubmit = (newLabel: string | undefined): void => {
+    setInputValues((prevState): any => ({ ...prevState, label: newLabel }))
+  }
+
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>): void =>
+    setInputValues((prevState): any => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
 
   const handlers = React.useMemo(
     () => ({
-      handleMyVisorsList: () => console.log('handleMyVisorsList'),
+      addNewVisor: (visor) => dispatch(addNewVisor(visor)),
     }),
-    []
-  );
+    [dispatch]
+  )
 
-  return { myVisorsList, handlers };
-};
+  return { myVisorsList, handlers }
+}
 
-export default useMyVisorsList;
+export default useMyVisorsList
