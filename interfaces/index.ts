246ba78bfe4f7785d84e5edeafc0ea49023b832e
madeinchema <chemavg1@gmail.com>
2021-05-06 13:23:30 +0200

    make label required in MyVisor type

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,17 +1,17 @@
-export type VisorKey = string;
-export type VisorLabel = string;
+export type VisorKey = string
+export type VisorLabel = string
 
 export type MyVisor = {
-  key: VisorKey;
-  label?: VisorLabel;
-};
+  key: VisorKey
+  label: VisorLabel
+}
 
 export type VisorUptime = {
-  key: VisorKey;
-  uptime: number;
-  downtime: number;
-  percentage: number;
-  online: boolean;
-};
+  key: VisorKey
+  uptime: number
+  downtime: number
+  percentage: number
+  online: boolean
+}
 
-export type MyVisorUptime = MyVisor & VisorUptime;
+export type MyVisorUptime = MyVisor & VisorUptime
