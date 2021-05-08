7e432ffcadfb22ecedc2cf6dda7e46efab10d912
madeinchema <chemavg1@gmail.com>
2021-05-08 21:35:32 +0200

    add features to handle initial data loading

--- components/modules/VisorsList.tsx
+++ components/modules/VisorsList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Heading, VStack } from '@chakra-ui/layout'
 import { Button } from '@chakra-ui/button'
 import useVisorsUptimeList from '../../hooks/useVisorsUptimeList'
@@ -6,26 +6,38 @@ import VisorsTable from '../VisorsTable'
 
 const VisorsList = (): JSX.Element => {
   const [showList, setShowList] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const { visorsUptimeList } = useVisorsUptimeList()
 
   const handleShowList = () => {
     setShowList((prevState) => !prevState)
   }
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1500)
+  }, [])
+
   return (
     <VStack spacing={4} w="100%">
       <Heading as="h1" size="lg">
         Visors List
       </Heading>
-      {showList ? (
+      {!isLoading && showList && (
         <>
           <Button size="sm" onClick={handleShowList}>
             Hide list
           </Button>
           <VisorsTable dataSource={visorsUptimeList} />
         </>
-      ) : (
-        <Button colorScheme="blue" onClick={handleShowList}>
+      )}
+      {!showList && (
+        <Button
+          isLoading={isLoading}
+          colorScheme="blue"
+          onClick={handleShowList}
+        >
           Show list
         </Button>
       )}
