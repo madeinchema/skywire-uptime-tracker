70d8b199588404a51e22247f6e923d54b7152928
madeinchema <chemavg1@gmail.com>
2021-05-08 21:26:18 +0200

    add Button to handle showList

--- components/modules/VisorsList.tsx
+++ components/modules/VisorsList.tsx
@@ -1,17 +1,34 @@
+import React, { useState } from 'react'
 import { Heading, VStack } from '@chakra-ui/layout'
-import React from 'react'
+import { Button } from '@chakra-ui/button'
 import useVisorsUptimeList from '../../hooks/useVisorsUptimeList'
 import VisorsTable from '../VisorsTable'
 
 const VisorsList = (): JSX.Element => {
+  const [showList, setShowList] = useState(false)
   const { visorsUptimeList } = useVisorsUptimeList()
+
+  const handleShowList = () => {
+    setShowList((prevState) => !prevState)
+  }
 
   return (
     <VStack spacing={4} w="100%">
       <Heading as="h1" size="lg">
         Visors List
       </Heading>
-      <VisorsTable dataSource={visorsUptimeList} />
+      {showList ? (
+        <>
+          <Button size="sm" onClick={handleShowList}>
+            Hide list
+          </Button>
+          <VisorsTable dataSource={visorsUptimeList} />
+        </>
+      ) : (
+        <Button colorScheme="blue" onClick={handleShowList}>
+          Show list
+        </Button>
+      )}
     </VStack>
   )
 }
