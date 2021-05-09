06ba8e1008fd42c3c09cefb51a4f3405c6dc42d4
madeinchema <chemavg1@gmail.com>
2021-05-09 18:32:09 +0200

    copy visor key on cell click & show a toast

--- components/common/VisorsTable/components/VisorKeyTableCell.tsx
+++ components/common/VisorsTable/components/VisorKeyTableCell.tsx
@@ -1,4 +1,6 @@
+import { useClipboard } from '@chakra-ui/hooks'
 import { Td } from '@chakra-ui/table'
+import { useToast } from '@chakra-ui/toast'
 import { VisorKey } from '../../../../interfaces'
 
 interface VisorKeyTableCellProps {
@@ -8,8 +10,19 @@ interface VisorKeyTableCellProps {
 const VisorKeyTableCell = ({
   visorKey,
 }: VisorKeyTableCellProps): JSX.Element => {
+  const toast = useToast()
+  const { onCopy } = useClipboard(visorKey)
+
+  const handleCopyVisorKey = (): void => {
+    onCopy()
+    toast({
+      title: 'The public key has been copied.',
+      status: 'success',
+      duration: 3000,
+    })
+  }
   return (
-    <Td wordBreak="break-all" minW="320px">
+    <Td wordBreak="break-all" minW="320px" onClick={handleCopyVisorKey}>
       {visorKey}
     </Td>
   )
