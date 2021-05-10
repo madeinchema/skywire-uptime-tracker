d5655bcda1b0692fcaea86f87882721d0370f2ad
madeinchema <madeinchema@gmail.com>
2021-05-10 10:21:35 +0200

    show text when there are no visors saved

--- components/modules/MyVisors/index.tsx
+++ components/modules/MyVisors/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, VStack } from '@chakra-ui/layout'
+import { Heading, Text, VStack } from '@chakra-ui/layout'
 import { Spinner } from '@chakra-ui/spinner'
 import React from 'react'
 import useMyVisors from '../../../hooks/useMyVisors'
@@ -10,13 +10,16 @@ const MyVisors = (): JSX.Element => {
     handlers: { updateVisorLabel },
   } = useMyVisors()
 
+  const isEmptyData = data?.length === 0
+
   return (
     <VStack spacing={4} w="100%">
       <Heading as="h1" size="lg">
         Your Visors
       </Heading>
-      {isLoading ? (
-        <Spinner />
+      {isLoading && <Spinner />}
+      {isEmptyData && !isLoading ? (
+        <Text>No visors have been added yet.</Text>
       ) : (
         <VisorsTable dataSource={data} onLabelSubmit={updateVisorLabel} />
       )}
