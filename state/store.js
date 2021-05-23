989d098c43bb8c9f5a758e280264832d86085353
madeinchema <chemavg1@gmail.com>
2021-05-23 21:10:03 +0200

    disable saga from the store

--- state/store.js
+++ state/store.js
@@ -1,17 +1,9 @@
-import {
-  combineReducers,
-  configureStore,
-  getDefaultMiddleware,
-} from '@reduxjs/toolkit'
-import createSagaMiddleware from 'redux-saga'
-import { rootSaga } from './sagas/rootSaga'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import visorsReducer from './slices/visorsSlice'
 import myVisorsReducer from './slices/myVisorsSlice'
 import checkedVisorReducer from './slices/checkVisorSlice'
 import toastsReducer from './slices/toastsSlice'
-
-const sagaMiddleware = createSagaMiddleware()
 
 const reducer = combineReducers({
   visors: visorsReducer,
@@ -20,9 +12,6 @@ const reducer = combineReducers({
   toasts: toastsReducer,
 })
 
-const middleware = [...getDefaultMiddleware(), sagaMiddleware]
 export default configureStore({
   reducer,
-  middleware,
 })
-sagaMiddleware.run(rootSaga)
