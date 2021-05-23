d0e88cac5637953b9c00469a75b5db7c9a8e0ae0
madeinchema <chemavg1@gmail.com>
2021-05-23 21:14:50 +0200

    remove addMyVisorSuccess reducer

--- state/slices/myVisorsSlice.ts
+++ state/slices/myVisorsSlice.ts
@@ -1,6 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { VisorKey, MyVisor, VisorLabel } from '../../interfaces/index'
 
+import { loadMyVisors } from '../thunks/myVisors/loadMyVisors'
+import { addMyVisor } from '../thunks/myVisors/addMyVisor'
+
 /**
  * State
  */
@@ -21,10 +24,6 @@ const initialState: MyVisorsState | [] = {
 /**
  * Actions' types
  */
-interface AddNewVisorAction {
-  type: string
-  payload: MyVisor
-}
 interface UpdateVisorAction {
   type: string
   payload: {
@@ -40,19 +39,6 @@ export const myVisorsSlice = createSlice({
   name: 'myVisors',
   initialState,
   reducers: {
-    loadMyVisors: () => {},
-    addMyVisor: () => {},
-    addMyVisorSuccess: {
-      reducer: (state, { payload }: AddNewVisorAction) => {
-        state.data.push(payload)
-      },
-      prepare: payload => ({
-        payload: {
-          ...payload,
-          label: payload.label || 'Visor',
-        },
-      }),
-    },
     updateVisorLabel: (
       state,
       { payload: { visorKey, label } }: UpdateVisorAction
@@ -72,14 +58,44 @@ export const myVisorsSlice = createSlice({
       state.data = state.data.filter(visor => visor.visorKey !== visorKey)
     },
   },
+  extraReducers: builder => {
+    builder.addCase(addMyVisor.pending, state => {
+      state.loading = true
+      state.success = undefined
+      state.error = undefined
+    })
+    builder.addCase(addMyVisor.fulfilled, (state, action) => {
+      if (action.payload) {
+        state.data.push(action.payload)
+      }
+      state.loading = false
+      state.success = true
+      state.error = undefined
+    })
+    builder.addCase(addMyVisor.rejected, (state, action) => {
+      state.loading = false
+      state.success = false
+      state.error = action.error.message
+    })
+
+    builder.addCase(loadMyVisors.pending, state => {
+      state.loading = true
+      state.success = undefined
+      state.error = undefined
+    })
+    builder.addCase(loadMyVisors.fulfilled, state => {
+      state.loading = false
+      state.success = true
+      state.error = undefined
+    })
+    builder.addCase(loadMyVisors.rejected, (state, action) => {
+      state.loading = false
+      state.success = false
+      state.error = action.error.message
+    })
+  },
 })
 
-export const {
-  loadMyVisors,
-  addMyVisor,
-  addMyVisorSuccess,
-  updateVisorLabel,
-  removeVisor,
-} = myVisorsSlice.actions
+export const { updateVisorLabel, removeVisor } = myVisorsSlice.actions
 
 export default myVisorsSlice.reducer
