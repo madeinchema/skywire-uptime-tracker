e523d0136e6773715ed3fb25bbc030c82f05a358
madeinchema <chemavg1@gmail.com>
2021-05-25 13:57:59 +0200

    disable eslint props-no-spreading rule

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { AppProps } from 'next/dist/next-server/lib/router/router';
-import { Provider } from 'react-redux';
-import store from '../state/store';
-import customTheme from '../theme/theme';
+/* eslint-disable react/jsx-props-no-spreading */
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { Provider } from 'react-redux'
+import store from '../state/store'
+import customTheme from '../theme/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider theme={customTheme}>
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
