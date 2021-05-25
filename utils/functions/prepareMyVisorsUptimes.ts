b3270930872d731453b3370af76a80bfcee4db37
madeinchema <chemavg1@gmail.com>
2021-05-25 22:51:20 +0200

    clean code and add types

--- utils/functions/prepareMyVisorsUptimes.ts
+++ utils/functions/prepareMyVisorsUptimes.ts
@@ -1,17 +1,20 @@
 import { MyVisor, MyVisorUptime, VisorUptime } from '../../interfaces'
 
-const getMyVisorsUptimes = (myVisors, visors): MyVisorUptime[] => {
-  const visorKeysToMatch = myVisors.map((myVisor: MyVisor) => myVisor.visorKey)
-  const matchingVisors = visors.filter((visor: VisorUptime) =>
-    visorKeysToMatch.includes(visor.visorKey)
-  )
-  const myVisorsUptimesWithLabels = matchingVisors.map((visor: VisorUptime) => {
-    const myVisorToUpdate = myVisors.find(
-      (myVisor: MyVisor) => myVisor.visorKey === visor.visorKey
+const getMyVisorsUptimes = (
+  myVisors: MyVisor[],
+  visors: VisorUptime[]
+): MyVisorUptime[] => {
+  const matchingVisors = visors.filter((visor: VisorUptime) => {
+    const matchingVisor = myVisors.find(
+      myVisor => myVisor.visorKey === visor.visorKey
     )
-    return myVisorToUpdate && { ...visor, label: myVisorToUpdate.label }
-  })
-  const sortedVisors = myVisorsUptimesWithLabels.sort(
+    if (!matchingVisor?.visorKey) {
+      return false
+    }
+    return { ...visor, label: matchingVisor.label }
+  }) as MyVisorUptime[]
+
+  const sortedVisors = matchingVisors.sort(
     (a: MyVisorUptime, b: MyVisorUptime) =>
       a.label.localeCompare(b.label, 'en', { numeric: true })
   )
