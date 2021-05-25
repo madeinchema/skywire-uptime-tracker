98514543809bed913f9e1c63d32abc20455e33b9
madeinchema <chemavg1@gmail.com>
2021-05-25 12:48:04 +0200

    add checkVisor thunk & update slice

--- state/slices/checkVisorSlice.ts
+++ state/slices/checkVisorSlice.ts
@@ -1,20 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { VisorUptime } from '../../interfaces'
+import { checkVisor } from '../thunks/checkVisor/checkVisor'
 
 /**
  * State
  */
-export type VisorsState = {
-  data: VisorUptime[]
+export type CheckedVisorState = {
+  data: VisorUptime | undefined
   loading: boolean
-  success: boolean | undefined
   error: string | undefined
 }
 
-const initialState: VisorsState = {
-  data: [],
+const initialState: CheckedVisorState = {
+  data: undefined,
   loading: false,
-  success: false,
   error: undefined,
 }
 
@@ -25,13 +24,29 @@ export const checkVisorSlice = createSlice({
   name: 'checkedVisor',
   initialState,
   reducers: {
-    checkVisor() {},
-    setCheckedVisor(state, { payload }) {
-      return { ...state, ...payload }
-    },
+    removeCheckedVisor: () => initialState,
+  },
+  extraReducers: builder => {
+    builder.addCase(checkVisor.pending, state => {
+      state.data = undefined
+      state.loading = true
+      state.error = undefined
+    })
+
+    builder.addCase(checkVisor.fulfilled, (state, action) => {
+      state.data = action.payload as VisorUptime
+      state.loading = false
+      state.error = undefined
+    })
+
+    builder.addCase(checkVisor.rejected, (state, action) => {
+      state.data = undefined
+      state.loading = false
+      state.error = action.error.message
+    })
   },
 })
 
-export const { checkVisor, setCheckedVisor } = checkVisorSlice.actions
+export const { removeCheckedVisor } = checkVisorSlice.actions
 
 export default checkVisorSlice.reducer
