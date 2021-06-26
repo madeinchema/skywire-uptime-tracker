94538cf41b71a1ace10c52dfea4241cf42cb52f4
madeinchema <chemavg1@gmail.com>
2021-06-26 23:44:59 +0200

    update sample data visorKey to key (latest API change)

--- utils/sample-visors-uptime-data.ts
+++ utils/sample-visors-uptime-data.ts
@@ -1,103 +1,90 @@
 export default [
   {
-    visorKey:
-      '020011587bf42a45b15f40d6783f5e5320a69a97a7298382103b754f2e3b6b63e9',
+    key: '020011587bf42a45b15f40d6783f5e5320a69a97a7298382103b754f2e3b6b63e9',
     uptime: 574251,
     downtime: 2104149,
     percentage: 21.44007616487455,
     online: false,
   },
   {
-    visorKey:
-      '02001728a88c27b6fa73ebc969dccdbcbd1d4393f267ea10fff2ed8d5eccaca0a4',
+    key: '02001728a88c27b6fa73ebc969dccdbcbd1d4393f267ea10fff2ed8d5eccaca0a4',
     uptime: 156291,
     downtime: 2522109,
     percentage: 5.835237455197133,
     online: true,
   },
   {
-    visorKey:
-      '02004a94f317f3a7f857b4531906e72a0691bf1853e07d17e6632e40240bb11ee1',
+    key: '02004a94f317f3a7f857b4531906e72a0691bf1853e07d17e6632e40240bb11ee1',
     uptime: 155400,
     downtime: 2523000,
     percentage: 5.801971326164875,
     online: true,
   },
   {
-    visorKey:
-      '02004aa9e2caea09fa20d9fb701737627e8df14a0c3ed082416f23857465982757',
+    key: '02004aa9e2caea09fa20d9fb701737627e8df14a0c3ed082416f23857465982757',
     uptime: 156291,
     downtime: 2522109,
     percentage: 5.835237455197133,
     online: true,
   },
   {
-    visorKey:
-      '02006c60e7ed61006c7e19aab75362f3b36ddfc0c5e6c845533470de1445023bae',
+    key: '02006c60e7ed61006c7e19aab75362f3b36ddfc0c5e6c845533470de1445023bae',
     uptime: 155400,
     downtime: 2523000,
     percentage: 5.801971326164875,
     online: true,
   },
   {
-    visorKey:
-      '020088ac4b5a8ffc8409e64e859e1966b21ca02b31605cfaf0b8dbb6242051b642',
+    key: '020088ac4b5a8ffc8409e64e859e1966b21ca02b31605cfaf0b8dbb6242051b642',
     uptime: 151920,
     downtime: 2526480,
     percentage: 5.672043010752688,
     online: true,
   },
   {
-    visorKey:
-      '0200adbb247a99bc321498ef31f654a126ca395a04a9bded5954a1236ed6532b14',
+    key: '0200adbb247a99bc321498ef31f654a126ca395a04a9bded5954a1236ed6532b14',
     uptime: 156240,
     downtime: 2522160,
     percentage: 5.833333333333333,
     online: true,
   },
   {
-    visorKey:
-      '0200ae2e5e6b16ce6c47c819dd9a9f8305057271146331b7844dda0fee88879087',
+    key: '0200ae2e5e6b16ce6c47c819dd9a9f8305057271146331b7844dda0fee88879087',
     uptime: 156291,
     downtime: 2522109,
     percentage: 5.835237455197133,
     online: true,
   },
   {
-    visorKey:
-      '0200ba5ab5553dec2cd15d56d57a15ae896f35bcbad2d64920229dea940494cd2b',
+    key: '0200ba5ab5553dec2cd15d56d57a15ae896f35bcbad2d64920229dea940494cd2b',
     uptime: 156291,
     downtime: 2522109,
     percentage: 5.835237455197133,
     online: true,
   },
   {
-    visorKey:
-      '0200be059f18379dd6ec133bd555170c7cbf6138181a92b0b23f6ba334c360feec',
+    key: '0200be059f18379dd6ec133bd555170c7cbf6138181a92b0b23f6ba334c360feec',
     uptime: 156291,
     downtime: 2522109,
     percentage: 5.835237455197133,
     online: true,
   },
   {
-    visorKey:
-      '0200c0ab8fb85c29d367805367de183e56e1388a1e8bc4a1bce9671a6db9ddadb5',
+    key: '0200c0ab8fb85c29d367805367de183e56e1388a1e8bc4a1bce9671a6db9ddadb5',
     uptime: 156291,
     downtime: 2522109,
     percentage: 5.835237455197133,
     online: true,
   },
   {
-    visorKey:
-      '0200c7c7e15d9f9b6abdf06c02a14df1653b79b40e8528d85a941bdd76fa2946d2',
+    key: '0200c7c7e15d9f9b6abdf06c02a14df1653b79b40e8528d85a941bdd76fa2946d2',
     uptime: 16980,
     downtime: 2661420,
     percentage: 0.6339605734767025,
     online: true,
   },
   {
-    visorKey:
-      '0200f3786c2522ba1185c682fa495d991f3db60803ad80c793bc17e4e17e666f62',
+    key: '0200f3786c2522ba1185c682fa495d991f3db60803ad80c793bc17e4e17e666f62',
     uptime: 156291,
     downtime: 2522109,
     percentage: 5.835237455197133,
